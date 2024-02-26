4991073eae28d11351120d26e2267d692b100b64
Fhandy Nofalino <144107184+kataparus14@users.noreply.github.com>
2024-02-26 16:53:32 +0700

    Add files via upload

---
+++ jobsheet 1/latihan/Untitled1.cpp
@@ -0,0 +1,9 @@
+//cretaed by fhandy nofalino akhsa 23343065
+
+#include <stdio.h>
+
+int main()
+{
+	printf("\n welcome to the word of C");//print the message on the screen
+	return 0;//return a value 0 to the operating system 
+}

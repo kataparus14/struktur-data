b26dbdce6a7e33c362adb6b22c4b469fe7d9b211
Fhandy Nofalino <144107184+kataparus14@users.noreply.github.com>
2024-03-25 20:33:27 +0700

    Delete jobsheet 1 directory

--- jobsheet 1/latihan/Untitled1.cpp
+++
@@ -1,9 +0,0 @@
-//cretaed by fhandy nofalino akhsa 23343065
-
-#include <stdio.h>
-
-int main()
-{
-	printf("\n welcome to the word of C");//print the message on the screen
-	return 0;//return a value 0 to the operating system 
-}
